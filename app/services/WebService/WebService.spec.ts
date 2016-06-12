2fd7d8f91a02b1f9931b5086a0cc7fb68e1899f4
Gelio <voreny.gelio@gmail.com>
2016-06-12 22:57:01 +0200

    added basic remove and edit tests

--- app/services/WebService/WebService.spec.ts
+++ app/services/WebService/WebService.spec.ts
@@ -1,23 +1,24 @@
 /// <reference path="../../../typings/main.d.ts" />
-const mockData = `{
-            "users": [
-                {
-                    "id": 1,
-                    "name": "Uma",
-                    "surname": "Ellison",
-                    "birthDate": "24-10-1984",
-                    "mobile": "653820575",
-                    "address": "3167 Augue Ave"
-                },
-                {
-                    "id": 2,
-                    "name": "Ila",
-                    "surname": "Hensley",
-                    "birthDate": "04-12-1989",
-                    "mobile": "390735720",
-                    "address": "Ap #994-6372 Feugiat St."
-                }
-                ]}`;
+const mockData = {
+    'users': [
+        {
+            'id': 1,
+            'name': 'Uma',
+            'surname': 'Ellison',
+            'birthDate': '24-10-1984',
+            'mobile': '653820575',
+            'address': '3167 Augue Ave'
+        },
+        {
+            'id': 2,
+            'name': 'Ila',
+            'surname': 'Hensley',
+            'birthDate': '04-12-1989',
+            'mobile': '390735720',
+            'address': 'Ap #994-6372 Feugiat St.'
+        }
+    ]
+};
 
 describe('WebService tests', function() {
     let $httpBackend: ng.IHttpBackendService,
@@ -45,7 +46,43 @@ describe('WebService tests', function() {
         $httpBackend.expectGET('users.json').respond(200, mockData);
 
         WebService.fetchInitialData().then(response => {
-            expect(JSON.stringify(response.data)).toEqual(JSON.stringify(JSON.parse(mockData)));
+            expect(JSON.stringify(response.data)).toEqual(JSON.stringify(mockData));
+            setTimeout(done, 0);    // make sure $digest from $httpBackend.flush is complete before exiting
+        });
+        $httpBackend.flush();
+    }, 5000);
+
+    it('should send a proper edit request', (done) => {
+        $httpBackend.expectPOST('http://users.impaqgroup.com/edit/1').respond(200);
+        let user = {
+            address: 'Woloska 22',
+            birthDate: '02-12-1936',
+            id: 1,
+            mobile: '123456789',
+            name: 'Lorem',
+            surname: 'Ipsum',
+        };
+
+        WebService.edit(user).then(response => {
+            expect(response.status).toEqual(200);
+            setTimeout(done, 0);    // make sure $digest from $httpBackend.flush is complete before exiting
+        });
+        $httpBackend.flush();
+    }, 5000);
+
+    it('should send a proper remove request', (done) => {
+        $httpBackend.expectPOST('http://users.impaqgroup.com/remove/1').respond(200);
+        let user = {
+            address: 'Woloska 22',
+            birthDate: '02-12-1936',
+            id: 1,
+            mobile: '123456789',
+            name: 'Lorem',
+            surname: 'Ipsum',
+        };
+
+        WebService.remove(user).then(response => {
+            expect(response.status).toEqual(200);
             setTimeout(done, 0);    // make sure $digest from $httpBackend.flush is complete before exiting
         });
         $httpBackend.flush();
